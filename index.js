210fdca8afba951368019fad73b34e57d171e973
Kauê Damasceno Farias <kauedamasfarias@gmail.com>
2024-08-26 09:52:49 -0300

    adding js in the project

---
+++ index.js
@@ -0,0 +1,62 @@
+// const
+const firstName = document.querySelector('#first_name')
+const lastName = document.querySelector('#last_name')
+const email = document.querySelector("#email")
+const textarea = document.querySelector("#textarea")
+const consent = document.querySelector("#consent")
+const errorFirstName = document.querySelector('#error_first_name')
+const errorLastName = document.querySelector('#error_last_name')
+const errorEmail = document.querySelector("#error_email")
+const errorTextarea = document.querySelector("#error_textarea")
+const errorConsent = document.querySelector("#error_consent")
+
+const submit = document.querySelector('.submit')
+
+// validate
+const validation = () => {
+  errorFirstName.textContent = '';
+  errorLastName.textContent = '';
+  errorEmail.textContent = '';
+  errorTextarea.textContent = ''
+  errorConsent.textContent = ''
+
+  let isValid = true
+
+  if (firstName.value == ''){
+  errorFirstName.textContent = 'this field is required'
+  isValid = false
+  }
+  
+  if (lastName.value == '') {
+    errorLastName.textContent = 'this field is required'
+    isValid = false
+  }
+
+  if (email.value == '' || !email.value.includes('@')) {
+    errorEmail.textContent = 'Please enter a valid email addres'
+    isValid = false
+  }
+
+  if (textarea.value == '') {
+    errorTextarea.textContent = 'This field is required'
+    isValid = false
+  }
+
+  if (!consent.checked) {
+    errorConsent.textContent = 'To submit this form, please content to being contacted'
+    isValid = false  
+  }
+
+
+  if (isValid){
+    return 'it is validate'
+  }
+  
+}
+
+//submit
+
+submit.addEventListener('click', (e) => {
+  e.preventDefault()
+  const validade = validation()
+})
